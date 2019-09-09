a01508aa00fe24502be61f39a4e489598daad364
Dan Smith <daniel.smith8795@gmail.com>
2019-09-09 12:32:29 -0500

    added in schema and seeds, started on connection to sql

---
+++ config/connection.js
@@ -0,0 +1,5 @@
+var path = require("path");
+
+module.exports = function (app) {
+    //insert code to connect node to mysql
+}
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers_db
+    (burgers)
+VALUES("cheeseburger"),
+    ("Double Bacon Burger"),
+    ("Vegi Burger"),
+    ("Mushroom Bacon Swiss");
