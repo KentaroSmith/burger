19337417649ad81cada3af5c05f2717faf9f9401
Dan Smith <daniel.smith8795@gmail.com>
2019-09-11 23:35:50 -0500

    added in db settings for heroku/ toyed with stylesheet

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var express = require("express");
 var handlebars = require("express-handlebars")
 var app = express();
-//var mysql = require("mysql");
+
 
 const PORT = process.env.PORT || 3000;
-//app.use(express.static("public"));
+app.use(express.static("public"));
 //data parsing 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
